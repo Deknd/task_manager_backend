2f10171ed7a900673626f76a1a4bf097760dc0db
denis <fellix.knd@gmail.com>
2023-08-07 17:25:13 +0400

    делаю рефакторинг кода

--- src/pages/TasksDashboard/TasksDashboard.jsx
+++ src/pages/TasksDashboard/TasksDashboard.jsx
@@ -2,22 +2,24 @@ import React from "react";
 import { UserPanelWidget, ButtonPanelWidget, ListTaskWidget } from '../../widgets'
 
 
-
+//главная страница для работы с тасками
 export const TasksDashboard = () => {
 
 
 
     return (
         <div>
-        <UserPanelWidget/>
-        <div style={{ display: 'flex',}}>
-          <div >
-             <ButtonPanelWidget/>
+          {/* Виджет для работы с данными пользователя (ok) */}
+          <UserPanelWidget/>
+          <div style={{ display: 'flex',}}>
+            <div >
+              {/* панель управление тасками */}
+              <ButtonPanelWidget/>
+            </div>
+            <div style={{ flex: 1}}>
+              <ListTaskWidget/>
+            </div>
           </div>
-          <div style={{ flex: 1}}>
-             <ListTaskWidget/>
-          </div>
-        </div>
 
         </div>
     )
